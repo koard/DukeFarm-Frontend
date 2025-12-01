f3e1fe5b419f75b551d82659e9ef0093c344735d
Ratchanon Kulpatrakorn <koardtep@gmail.com>
2025-12-01 21:39:20 +0700

    feat: add remote image patterns for profile and placeholder images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   //output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'profile.line-scdn.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+    ],
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
